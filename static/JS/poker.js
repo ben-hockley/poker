64457ab2235c34800059e0e624f6343178791fe7
c23037255 <HockleyB@cardiff.ac.uk>
2024-03-28 22:48:00 +0000

    fixed array on game reset

--- static/JS/poker.js
+++ static/JS/poker.js
@@ -1,14 +1,11 @@
-//spades,clubs,hearts,diamonds
-deck = [
-    '2S','3S','4S','5S','6S','7S','8S','9S','TS','JS','QS','KS','AS',
-    '2C','3C','4C','5C','6C','7C','8C','9C','TC','JC','QC','KC','AC',
-    '2H','3H','4H','5H','6H','7H','8H','9H','TH','JH','QH','KH','AH',
-    '2D','3D','4D','5D','6D','7D','8D','9D','TD','JD','QD','KD','AD'
-]
-
-
 //shuffles deck on page load.
 function shuffle(){ 
+    deck = [
+        '2S','3S','4S','5S','6S','7S','8S','9S','TS','JS','QS','KS','AS',
+        '2C','3C','4C','5C','6C','7C','8C','9C','TC','JC','QC','KC','AC',
+        '2H','3H','4H','5H','6H','7H','8H','9H','TH','JH','QH','KH','AH',
+        '2D','3D','4D','5D','6D','7D','8D','9D','TD','JD','QD','KD','AD'
+    ]
     shuffledDeck = []
     while (deck.length > 0){
         randomIndex = Math.floor(Math.random() * deck.length);
@@ -106,11 +103,13 @@ function setupGame(){
 
     roundsPlayed = 0;
     roundsPlayedIncrementer = 0.25;
+    gameStage = 0;
 
     document.getElementById('cpu1').innerHTML = '';
     document.getElementById('cpu2').innerHTML = '';
     document.getElementById('cpu3').innerHTML = '';
     document.getElementById('player').innerHTML = '';
+    document.getElementById('cardsOnTheTable').innerHTML = '';
 
     order = ['cpu1', 'cpu2', 'cpu3', 'player'];
     bet = [0, 0, 0, 0];
